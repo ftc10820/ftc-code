b84efee819ec2831eeff47f9914bc648f927011a
mmshah2004 <mmshah2004@gmail.com>
2017-01-03 19:30:48 -0500

    Update ScorpioTele.java

--- internal/ScorpioTele.java
+++ internal/ScorpioTele.java
@@ -2,16 +2,15 @@ package org.firstinspires.ftc.robotcontroller.internal;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-
-import static java.lang.Thread.sleep;
-
 
 /**
  * Created by the Falconeers 10820
  */
-public class ScorpioTele extends OpMode{
+
+public class ScorpioTele extends OpMode {
 
     private DcMotor frontLeft;
     private DcMotor frontRight;
@@ -23,6 +22,9 @@ public class ScorpioTele extends OpMode{
     private Servo pushR;
     private Servo pushL;
     private TouchSensor touchSensor;
+    double startPos = 0;
+    //private GyroSensor gyroSensor;
+
     @Override
     public void init() {
         frontLeft = hardwareMap.dcMotor.get("motor_frontLeft");
@@ -35,6 +37,16 @@ public class ScorpioTele extends OpMode{
         pushL = hardwareMap.servo.get("LeftBeaconPusher");
         pushR = hardwareMap.servo.get("RightBeaconPusher");
         touchSensor = hardwareMap.touchSensor.get("SweeperLimitSwitch");
+        //gyroSensor = hardwareMap.gyroSensor.get("");
+
+        pushR.setDirection(Servo.Direction.REVERSE);
+        pushL.setDirection(Servo.Direction.FORWARD);
+
+        pushR.scaleRange(0.5, 0.75);
+        pushL.scaleRange(0.5, 0.75);
+
+        pushL.setPosition(0);
+        pushR.setPosition(0);
 
     }
 
@@ -44,37 +56,69 @@ public class ScorpioTele extends OpMode{
         double backL = 0;
         double frontR = 0;
         double backR = 0;
-
+        double theta;
         double velocity = 1;
         double v0 = 0;
         double pi = 3.1459265359;
-        double piDiv4 = pi/4;
-        double theta = pi/2 + Math.atan2(gamepad1.right_stick_y,gamepad1.right_stick_x);
-        if(gamepad1.right_bumper) {
-            frontL = velocity * Math.sin(theta + piDiv4) + v0;
-            backL = velocity * Math.cos(theta + piDiv4) + v0;
-            frontR = velocity * Math.cos(theta + piDiv4) + v0;
-            backR =  velocity * Math.sin(theta + piDiv4) + v0;
-        }else stopMoving();
+        double piDiv4 = pi / 4;
+        boolean isRBPressed = gamepad1.right_bumper;
 
+        telemetry.addData("pushL pos", pushL.getPosition());
+        telemetry.addData("pushR pos", pushR.getPosition());
+
+
+        // Forward - -2.3572....
+        // Backward - 0.7843....
+        // Left - 2.35....
+        // Right - -0.786....
+
+
+//        telemetry.addData("x", gamepad1.right_stick_x);
+//        telemetry.addData("y", gamepad1.right_stick_y);
+        if (isRBPressed) {
+            if (Math.abs(gamepad1.right_stick_x) >= .1 || Math.abs(gamepad1.right_stick_y) >= .1) {
+                theta = Math.atan2(-gamepad1.right_stick_y, gamepad1.right_stick_x);
+                while(theta < 0) theta += Math.PI * 2;
+                telemetry.addData("theta", theta);
+                frontL = -velocity * Math.sin(theta + piDiv4) + v0;
+                backL = velocity * Math.cos(theta + piDiv4) + v0;
+                frontR = -velocity * Math.cos(theta + piDiv4) + v0;
+                backR = velocity * Math.sin(theta + piDiv4) + v0;
+            }
+        } else {
+            frontL = gamepad1.right_stick_y;
+            backL = gamepad1.right_stick_y;
+            frontR = -gamepad1.left_stick_y;
+            backR = -gamepad1.left_stick_y;
+        }
         sweep();
-        double largestVal =  Math.max(Math.max(Math.abs(frontL),Math.abs(backL)), (Math.max(Math.abs(frontR),Math.abs(backR))));
-        if(largestVal > 1){
+        double largestVal = Math.max(Math.max(Math.abs(frontL), Math.abs(backL)), (Math.max(Math.abs(frontR), Math.abs(backR))));
+        if (largestVal > 1) {
             frontL /= largestVal;
             backL /= largestVal;
             frontR /= largestVal;
             backR /= largestVal;
         }
 
-        if(gamepad1.right_trigger >= 0.1) launchBall();
-        double degServo = 0;
-        double startPos = 0;
-        if(gamepad2.right_bumper) pushR.setPosition(startPos + degServo);
-        if(gamepad2.left_bumper) pushL.setPosition(startPos - degServo);
+        if (gamepad2.right_trigger >= 0.1) launchBall();
+        double degServo = gamepad2.left_stick_x;
+        telemetry.addData("degServo ", degServo);
 
 
-            if (gamepad1.left_bumper) der();
-            else stopMoving();
+        if (gamepad2.right_bumper) {
+            pushL.setPosition(1);
+            waitTime(1000);
+            pushL.setPosition(0);
+        }
+        if (gamepad2.left_bumper) {
+            pushL.setPosition(1);
+            waitTime(1000);
+            pushL.setPosition(0);
+        }
+
+
+//            if (gamepad1.left_bumper) der();
+//            //else stopMoving();
 
         frontLeft.setPower(frontL);
         backLeft.setPower(backL);
@@ -83,29 +127,44 @@ public class ScorpioTele extends OpMode{
 
     }
 
-    private void der(){
-        double velocity = 1;
-        double pi = 3.1459265359;
-        double piDiv4 = pi/4;
-        double theta = pi/2 + Math.atan2(gamepad1.right_stick_y,gamepad1.right_stick_x);
-        frontLeft.setPower(velocity * Math.sin(theta + piDiv4));
-        backLeft.setPower(velocity * Math.cos(theta + piDiv4));
-        frontRight.setPower(velocity * Math.cos(theta + piDiv4));
-        backRight.setPower(velocity * Math.sin(theta + piDiv4));
-    }
 
-    private void launchBall(){
+//    private void der(){
+//        double velocity = 1;
+//        //double pi = 3.1459265359;
+//        double piDiv4 = Math.PI / 4;
+//        double theta = -Math.PI / 2 + Math.atan2(gamepad1.right_stick_y,gamepad1.right_stick_x);
+//        double frontL = 0;
+//        double backL = 0;
+//        double frontR = 0;
+//        double backR = 0;
+//        if(gamepad1.right_bumper) {
+//            frontL = velocity * Math.sin(theta + piDiv4);
+//            backL = velocity * Math.cos(theta + piDiv4);
+//            frontR = velocity * Math.cos(theta + piDiv4);
+//            backR =  velocity * Math.sin(theta + piDiv4);
+//        }
+//        double largestVal =  Math.max(Math.max(Math.abs(frontL),Math.abs(backL)), (Math.max(Math.abs(frontR),Math.abs(backR))));
+//        if(largestVal > 1){
+//            frontL /= largestVal;
+//            backL /= largestVal;
+//            frontR /= largestVal;
+//            backR /= largestVal;
+//        }
+//    }
+
+    private void launchBall() {
         double powerMod = gamepad2.right_trigger;
-        if(gamepad2.x) {
+        if (gamepad2.x) {
             flywheelLeft.setPower(-powerMod);
             flywheelRight.setPower(powerMod);
         }
     }
-    private void sweep(){
+
+    private void sweep() {
         sweeper.setPower(-gamepad2.left_trigger);
-        if(touchSensor.isPressed()) {
+        if (gamepad2.a) {
             sweeper.setPower(gamepad2.left_trigger);
-            telemetry.addData("sweeper: ",sweeper.getCurrentPosition());
+            telemetry.addData("sweeper: ", sweeper.getCurrentPosition());
         }
 //        try {
 //            sleep(1500);
@@ -114,10 +173,19 @@ public class ScorpioTele extends OpMode{
 //        }
 //        sweeper.setPower(0);
     }
-    private void stopMoving(){
+
+    private void stopMoving() {
         frontLeft.setPower(0);
         backLeft.setPower(0);
         frontRight.setPower(0);
         backRight.setPower(0);
+    }
+    private void waitTime(long time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+    }
+
 }
